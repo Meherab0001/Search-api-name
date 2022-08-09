b6d59bb6c2c4f0b0b766e20976fbf1c5356f0f1d
Meherab211 <meherab395@gmail.com>
2022-08-09 14:30:08 +0600

    added seacrh

---
+++ src/Components/ApiCard/ApiCard.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Card from './Card';
+
+const ApiCard = () => {
+    const [cards, setCards] = useState([])
+    const [searchText, setSearchText] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/comments')
+            .then(res => res.json())
+            .then(data => {
+                setCards(data)
+                setSearchText(data)
+            })
+    }, [])
+
+
+    function searchHandle(event) {
+        const searchData = event.target.value;
+        const filtered = cards.filter(card => card.name.toLowerCase().includes(searchData));
+        setSearchText(filtered);
+       
+    }
+    return (
+        <div>
+            <div className='bg-yellow-300 text-center'>
+                <input name='search' onChange={searchHandle} className='rounded p-2' type="text" placeholder='Search here name' />
+                <button class="ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Seacrh
+                </button>
+            </div>
+            <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-12 my-12'>
+
+
+              {/*   {
+                    cards?.map((card, index) => <Card
+                        card={card}
+                        index={index}
+                    ></Card>)
+                } */}
+                {
+                    searchText?.map((card, index) => <Card
+                        card={card}
+                        index={index}
+                    ></Card>)
+                }
+
+                
+              
+            </div>
+        </div>
+    );
+};
+
+export default ApiCard;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ApiCard from '../ApiCard/ApiCard';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This home page</h1>
+            <ApiCard></ApiCard>
+        </div>
+    );
+};
+
+export default Home;
